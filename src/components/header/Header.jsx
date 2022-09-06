8f7ec0649e7b588ece528ba90478136dec01ca36
arasaa <arazsaydu@gmail.com>
2022-09-06 14:42:14 +0200

    finished general css style

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import "./header.css"
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Araz Saydu</h1>
+        <h5 className='text-light'>Fullstack Webdeveloper</h5>
+        
+      </div>
+    </header>
   )
 }
 
