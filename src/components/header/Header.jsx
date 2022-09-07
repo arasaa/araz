343959d4a23c6114590bb6b52b33f1e96fd440d5
arasaa <arazsaydu@gmail.com>
2022-09-07 11:51:47 +0200

    added picture&scrolldown&socialmedia likes and icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./header.css"
+import CTA from './CTA'
+import pic2 from '../../assets/pic2.jpg'
+import HeaderSocial from './HeaderSocial'
 function Header() {
   return (
     <header>
@@ -7,7 +10,14 @@ function Header() {
         <h5>Hello I'm</h5>
         <h1>Araz Saydu</h1>
         <h5 className='text-light'>Fullstack Webdeveloper</h5>
-        
+        <CTA />
+
+        <div className='me'>
+          <img src={pic2} alt='' />
+        </div>
+
+        <a href='#contact' className='scroll-down'>Scroll Down</a>
+        <HeaderSocial />
       </div>
     </header>
   )
---
+++ src/components/header/HeaderSocial.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocial = () => {
+  return (
+    <div className='header-social'>
+        < a href='https://www.linkedin.com/in/araz-saydu-594940198/' target="blank">LinkedIn</a>
+        < a href='https://github.com/arasaa' target="blank">Github</a>
+        < a href='https://www.linkedin.com/in/araz-saydu-594940198/' target="blank">LinkedIn</a>
+    </div>
+  )
+}
+
+export default HeaderSocial
