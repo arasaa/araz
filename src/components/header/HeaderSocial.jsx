4cbb35eef518a1180d6a864e98bfce52d8da8a85
arasaa <arazsaydu@gmail.com>
2022-09-21 12:11:53 +0200

    did media query and added icons to nav

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -4,7 +4,7 @@ import {BsGithub} from 'react-icons/bs'
 
 const HeaderSocial = () => {
   return (
-    <div className='header-social'>
+    <div className='header-socials'>
         < a href='https://www.linkedin.com/in/araz-saydu-594940198/' target="blank"><BsLinkedin/></a>
         < a href='https://github.com/arasaa' target="blank"><BsGithub/></a>
         < a href='https://www.linkedin.com/in/araz-saydu-594940198/' target="blank"></a>
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import "./nav.css"
+import{AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageDots} from 'react-icons/bi'
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+        <a href='#'><AiOutlineHome /></a>
+        <a href='#about'><AiOutlineUser/></a>
+        <a href='#experience'><BiBook /></a>
+        <a href='services'><RiServiceLine /></a>
+        <a href='#contact'><BiMessageDots /></a>
+    </nav>
   )
 }
 
