0eb60e04248c33a698206bd747a95255516c26c8
arasaa <arazsaydu@gmail.com>
2022-09-24 12:12:50 +0200

    finshed the experience page

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,104 @@
 import React from 'react'
 import "./experience.css"
+import {AiFillCheckCircle} from 'react-icons/ai'
 
 function Experience() {
   return (
-    <section id='experience'>experience</section>
+    <section id='experience'>
+      <h5>Skils I have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience-container">
+        <div className="experience-frontend">
+            <h3>Frontend Development</h3>
+            <div className="experience-content">
+              <article className='experience-details'>
+                <AiFillCheckCircle className='experience-details-icon'/>
+                <div>
+                <h4>HTML</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+              <article className='experience-details'>
+                <AiFillCheckCircle className='experience-details-icon' />
+                <div>
+                <h4>CSS</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+              <article className='experience-details'>
+                <AiFillCheckCircle className='experience-details-icon' />
+               <div>
+               <h4>Bootstrap</h4>
+                  <small className="text-light">Intermediate</small>
+               </div>
+              </article>
+              <article className='experience-details'>
+                <AiFillCheckCircle className='experience-details-icon' />
+               <div>
+               <h4>SASS</h4>
+                  <small className="text-light">Intermediate</small>
+               </div>
+              </article>
+              <article className='experience-details'>
+                <AiFillCheckCircle className='experience-details-icon' />
+              <div>
+              <h4>JAVASCRIPT</h4>
+                  <small className="text-light">Experienced</small>
+              </div>
+              </article>
+              <article className='experience-details'>
+                <AiFillCheckCircle className='experience-details-icon' />
+                <div>
+                <h4>REACT</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+            </div>
+        </div>
+        <div className="experience-backend">
+            <h3>Backend Development</h3>
+            <div className="experience-content">
+              <article className='experience-details'>
+                <AiFillCheckCircle className='experience-details-icon' />
+                <div>
+                <h4>Node JS</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+                
+              </article>
+              <article className='experience-details'>
+                <AiFillCheckCircle className='experience-details-icon' />
+                <div>
+                <h4>ExpressJS</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+              <article className='experience-details'>
+                <AiFillCheckCircle className='experience-details-icon' />
+                <div>
+                <h4>MongoDB</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
+              <article className='experience-details'>
+                <AiFillCheckCircle className='experience-details-icon' />
+                <div>
+                <h4>Mongoose</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
+              <article className='experience-details'>
+                <AiFillCheckCircle className='experience-details-icon' />
+               <div>
+               <h4>SQL</h4>
+                  <small className="text-light">Intermediate</small>
+               </div>
+              </article>
+            </div>
+        </div>
+        </div>
+    </section>
   )
 }
 
